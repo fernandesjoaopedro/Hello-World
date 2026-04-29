6a4979777556ed0ad702c3db8c3b4ec350e220da
fernandesjoaopedro <joaoknd29@gmail.com>
2026-04-29 20:18:10 -0300

    Creation of personal-data.py

---
+++ personal-data.py
@@ -0,0 +1,5 @@
+name = input('Whats your name? ')
+age = int(input('How old are you? '))
+home = input('Where do you live? ')
+
+print(f'Your name is {name}, you are {age} years old and you live in {home}.')
