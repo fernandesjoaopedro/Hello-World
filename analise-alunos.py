3dad46165034ae1c9d5e87bdb41a467c457816f1
João Pedro Fernandes <151982334+fernandesjoaopedro@users.noreply.github.com>
2026-05-01 15:44:47 -0300

    Delete analise-alunos.py

--- analise-alunos.py
+++
@@ -1,9 +0,0 @@
-n1 = float(input("Type your first note: "))
-n2 = float(input("Type your second note: "))
-
-average = (n1 + n2)/2
-
-if average >= 6:
-    print(f'Your average was {average:.2f}, you were approved!')
-else:
-    print(f'Your average was {average:.2f}, you were not approved!')
